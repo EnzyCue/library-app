52d75bee3702d271d1716c16496d59357215c1d3
EnzyCue <mohammadalhakim787@gmail.com>
2023-02-20 16:56:41 +1100

    Add basic elements for title and card into html and css, implement foundation of implementLibrary function in JS

--- javascript.js
+++ javascript.js
@@ -13,13 +13,28 @@ function Book(title, author, numOfPages, isRead) {
 
 }
 
-
-const test = new Book('life of pis', 'pi baker', '314159', false);
-
-console.log(test.info());
-
-let myLibrary = [];
+const myLibrary = [];
 
 function addBookToLibrary(book){
     myLibrary.push(book);
+}
+
+addBookToLibrary(new Book('life of a pi', 'pi baker', '314159', false));
+addBookToLibrary(new Book('Shronk', 'Simon Baldwick', '78', true));
+addBookToLibrary(new Book('How to write code good', 'Good Coder', '289', false));
+addBookToLibrary(new Book('How to write code bad', 'Anonymous', '15', true));
+addBookToLibrary(new Book('Shronk 2', 'Simon Baldwick', '100', true));
+addBookToLibrary(new Book('Shronk 3', 'Simon Baldwick', '80', true));
+addBookToLibrary(new Book('Shronk: A New World.', 'Simon Baldwick', '120', false));
+
+console.log(myLibrary);
+
+function implementLibrary(library){
+    for(let i in library) {
+        const card = document.createElement('div');
+        card.classList.add('.card');
+
+        const mainContent = document.querySelector('.mainContainer');
+        mainContent.appendChild(card);
+    }
 }
